cf60cff2bc1ff31f3845336598afd5f70eeb4e1f
Chandima Ranaweera <chan4lk@gmail.com>
2020-07-14 15:01:47 +0530

    add base component

---
+++ libs/components/src/lib/pagination/pagination.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'angular-tdd-pagination',
+  templateUrl: './pagination.component.html',
+  styleUrls: ['./pagination.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class PaginationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
